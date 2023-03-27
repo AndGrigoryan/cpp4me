16279239572596e3df8a419c3d7d9b65675d2ec3
Andranik Grigoryan <andranik.grigoryan.02@gmail,com>
2023-03-27 18:32:57 +0400

    copy_constructors

--- Project_Beryl/Project_Beryl.cpp
+++ Project_Beryl/Project_Beryl.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
 
+using namespace std;
+
+struct IntArray
+{
+	IntArray() : size_(0), data_(nullptr)
+	{
+		cout << "Default constructor: " << this << endl;
+	}
+	
+	IntArray(int size) : size_(size), data_(new int[size_])
+	{
+		for (size_t i = 0; i < size_; i++)
+		{
+			this->data_[i] = i + 1;
+		}
+		cout << "Constructor: " << this << endl;
+	}
+
+	~IntArray()
+	{
+		delete[] data_;
+		cout << "Destructor: " << this << endl;
+	}
+
+	IntArray(IntArray const& other) : size_(other.size_), data_(new int[size_])
+	{
+		for (size_t i = 0; i < size_; i++)
+		{
+			this->data_[i] = other.data_[i];
+		}
+
+		cout << "Copy constructor: " << this << endl;
+	}
+
+	IntArray& operator =(IntArray const& other)
+	{
+		this->size_ = other.size_;
+		this->data_ = new int[size_];
+		for (size_t i = 0; i < size_; i++)
+		{
+			this->data_[i] = other.data_[i];
+		}
+	}
+
+	void print()
+	{
+		for (size_t i = 0; i < size_; i++)
+		{
+			cout << this->data_[i] << " ";
+		}
+		cout << endl;
+	}
+
+private:
+	int size_;
+	int* data_;
+};
+
 int main() 
 {
+	IntArray a1(20);
+	a1.print();
 
+	IntArray a2;
+	a2 = a1;
+	a2.print();
 
     return 0; 
 } 
